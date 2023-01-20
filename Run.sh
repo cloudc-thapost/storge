5eb391c783c623c2d8f8c52fab805067f1165e2b
cloudc-thapost <121788145+cloudc-thapost@users.noreply.github.com>
2023-01-20 11:55:10 +0700

    Create Run.sh

---
+++ Run.sh
@@ -0,0 +1,24 @@
+sudo apt-get update && apt-get install qemu -y
+sudo apt install qemu-utils -y
+sudo apt install qemu-system-x86-xen -y
+sudo apt install qemu-system-x86 -y
+wget -O 20GB.qcow2 'https://www.dropbox.com/s/6yzobwz46tzti0l/20GB.qcow2?dl=1'
+wget -O RTL8139F.iso 'https://drive.google.com/uc?export=download&id=1wDL8vo9mmYKw1HKXZzaYHoKmzSt_wXai'
+wget -O windows7_super-nano_lite.iso 'https://download1488.mediafire.com/h9bn32erf0tg/land087j09xszt0/Windows7_super-nano_lite.iso'
+
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+sudo qemu-system-x86_64 \
+  -m 8G \
+  -cpu EPYC \
+  -enable-kvm \
+  -boot order=d \
+  -drive file=windows7_super-nano_lite.iso,media=cdrom \
+  -drive file=20GB.qcow2 \
+  -drive file=RTL8139F.iso,media=cdrom \
+  -device usb-ehci,id=usb,bus=pci.0,addr=0x4 \
+  -device usb-tablet \
+  -vnc :0 \
+  -cpu core2duo \
+  -smp cores=2 \
+  -device rtl8139,netdev=n0 -netdev user,id=n0 \
+  -accel tcg,thread=multi \
