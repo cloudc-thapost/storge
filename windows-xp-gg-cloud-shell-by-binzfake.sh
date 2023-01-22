c43d68098f209c568222bb334e46c5b8c4322bfb
cloudc-thapost <121788145+cloudc-thapost@users.noreply.github.com>
2023-01-22 17:42:20 +0700

    Update windows-xp-gg-cloud-shell-by-binzfake.sh

--- windows-xp-gg-cloud-shell-by-binzfake.sh
+++ windows-xp-gg-cloud-shell-by-binzfake.sh
@@ -3,20 +3,35 @@ unzip -o ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 read -p "Enter your ngork token here: " CRP
 ./ngrok authtoken $CRP 
 nohup ./ngrok tcp 5900 &>/dev/null &
+
+
+
+clear
 echo "Script by BZOT9 TEAM"
 echo "Updating System..."
 sudo apt-get update -y
+clear
 echo "Setting Up..."
 sudo apt-get install -y qemu-system-x86-64
-echo "Download Disk Windows XP SP2 by BinZ FAKE"
+clear
+echo "Download Disk Windows XP SP3 by BinZ FAKE"
 wget -O XP_BY_BZOT9.qcow2 'https://www.dropbox.com/s/ogt3hnf0ghzgqij/XP_BY_BZOT9.qcow2?dl=1'
+clear
 echo "Download..."
 wget -O binz_disk_cd.iso 'https://www.dropbox.com/s/2h1r9z7tltkn4g6/binz_disk_cd.iso?dl=1'
-echo "Set up VNC address"
-
+clear
+echo "Set up VNC address..."
+sleep 5
+clear
 echo "Use VNC Viewer to connect."
 echo "Your Address VNC (Password login: binzfakevps) is:"
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "==================="
+echo "YT: youtube.com/@binzfake2500"
+echo "Website: binzfake9999.github.io"
+echo "Website: binzfake9999.github.io"
+echo "Email: bzot9ws@gmail.com"
+echo "==================="
 sudo qemu-system-x86_64 \
   -m 8G \
   -cpu EPYC \
